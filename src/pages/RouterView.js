1a4e71fc7d2c74a63a994b395ea29744e528f008
Franciszek Madej <f.madej@protonmail.com>
2019-05-04 19:51:19 +0200

    refactor: move content layout to per route setting

--- src/pages/RouterView.js
+++ src/pages/RouterView.js
@@ -9,55 +9,21 @@ import NavigationBar from "../components/NavigationBar";
 
 import AvailabilityView from "./AvailabilityView";
 import CaptainView from "./CaptainView";
-// import PersonalSettingsView from "./PersonalSettingsView";
+import PersonalSettingsView from "./PersonalSettingsView";
 // import TeamSettingsView from "./TeamSettingsView";
-
-const breakpoints = [400, 576, 1000, 1400];
-
-const mq = breakpoints.map(bp => `@media (min-width: ${bp}px)`);
-const gridTemplateColumnsCount = breakpoints.map(bp => {
-  let repeatedString = "";
-  let times = bp / 200;
-  while (times > 0) {
-    repeatedString += "1fr ";
-    times--;
-  }
-
-  return repeatedString;
-});
 
 const RouterView = () => {
   return (
-    <main
-      css={{
-        paddingTop: 100,
-        width: "100%",
-        display: "grid",
-        justifyItems: "center",
-        [mq[0]]: {
-          gridTemplateColumns: gridTemplateColumnsCount[0]
-        },
-        [mq[1]]: {
-          gridTemplateColumns: gridTemplateColumnsCount[1]
-        },
-        [mq[2]]: {
-          gridTemplateColumns: gridTemplateColumnsCount[2]
-        },
-        [mq[3]]: {
-          gridTemplateColumns: gridTemplateColumnsCount[3]
-        }
-      }}
-    >
-      <Router>
-        <Provider>
-          <Subscribe to={[LoginContainer]}>
-            {login => <NavigationBar login={login} />}
-          </Subscribe>
-        </Provider>
-        <Route path="/availability" component={AvailabilityView} />
-        <Route path="/captain" component={CaptainView} />
-      </Router>
-    </main>
+    <Router>
+      <Provider>
+        <Subscribe to={[LoginContainer]}>
+          {login => <NavigationBar login={login} />}
+        </Subscribe>
+      </Provider>
+      <Route path="/availability" component={AvailabilityView} />
+      <Route path="/captain" component={CaptainView} />
+      <Route path="/settings" component={PersonalSettingsView} />
+    </Router>
   );
 };
 
