d865c1b9347aeaa7a29cb0c4d0ec07516c7956ad
Franciszek Madej <f.madej@protonmail.com>
2019-05-14 21:26:36 +0200

    feat: allow SVGButton to render text
    rename might be beneficial, as SVGButton is now simply
    a base button for a project

--- src/components/common/SVGButton.js
+++ src/components/common/SVGButton.js
@@ -1,10 +1,11 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import { SuccessGreen } from "components/common/styles/Colors";
+import { SuccessGreen, TextBlack } from "components/common/styles/Colors";
 
 const SVGButton = props => {
   const ButtonStyle = css({
     backgroundColor: props.backgroundColor || SuccessGreen,
+    color: props.color || TextBlack,
     width: props.width || "auto",
     height: props.height || "auto",
     border: "none"
@@ -15,11 +16,15 @@ const SVGButton = props => {
       onClick={props.onClick}
       type={props.type}
     >
-      <img
-        css={{ display: "block", margin: "auto" }}
-        src={props.image}
-        alt={props.altText}
-      />
+      {props.image ? (
+        <img
+          css={{ display: "block", margin: "auto" }}
+          src={props.image}
+          alt={props.altText}
+        />
+      ) : (
+        props.children
+      )}
     </button>
   );
 };
