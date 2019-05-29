5b08918acb63227194508b315fb29995fce07d9b
Franciszek Madej <f.madej@protonmail.com>
2019-05-29 17:58:51 +0200

    test: add missing App.js tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,40 @@
 import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { Provider } from "unstated";
 import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<App />", () => {
   it("should mount", () => {
-    const wrapper = Enzyme.mount(
-      <App />
+    const login = {
+      state: { userLoggedIn: false },
+      setState: jest.fn(),
+      subscribe: jest.fn(),
+      unsubscribe: jest.fn()
+    };
+    const wrapper = Enzyme.shallow(
+      <Provider inject={[login]}>
+        <App />
+      </Provider>
     );
     expect(wrapper.find(App)).toBeTruthy();
+    expect(wrapper.find("LoginView")).toBeTruthy();
+  });
+  it("should show RouterView", () => {
+    const login = {
+      state: { userLoggedIn: true },
+      setState: jest.fn(),
+      subscribe: jest.fn(),
+      unsubscribe: jest.fn()
+    };
+    const wrapper = Enzyme.shallow(
+      <Provider inject={[login]}>
+        <App />
+      </Provider>
+    );
+    expect(wrapper.find(App)).toBeTruthy();
+    expect(wrapper.find("RouterView")).toBeTruthy();
   });
 });
-
